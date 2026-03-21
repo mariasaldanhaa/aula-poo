a49adc47e13e07b31a8ca3b878fa37ba17e549b2
Maria <mariasaldanha124@gmail.com>
2026-03-21 17:02:12 -0300

    feat: os cinco exercícios de Java concluídos

---
+++ exercicio-java/src/ex4/ex4.java
@@ -0,0 +1,19 @@
+package ex4;
+
+import java.util.Scanner;
+
+
+public class ex4 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite a temperatura em graus Celsius: ");
+		int celsius = scan.nextInt();
+		
+		float fahrenheit = (9 * celsius + 160) / 5;
+		
+		System.out.printf("Temperatura em Fahrenheit: %.2f", fahrenheit);
+		
+		scan.close();
+	}
+}
