1b4e721b2d80f634357521f714171f04490d00ca
Maria <mariasaldanha124@gmail.com>
2026-03-23 09:41:38 -0300

    feat: foram feitos os exercícios do 10 ao 15 em Java

---
+++ exercicio-java/src/ex12/ex12.java
@@ -0,0 +1,18 @@
+package ex12;
+
+import java.util.Scanner;
+
+public class ex12 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número qualquer: ");
+		int number = scan.nextInt();
+		
+		if(number > 20) {
+			System.out.printf("Número digitado: %d", number);
+		}
+		
+		scan.close();
+	}
+}
---
+++ exercicio-java/src/ex13/ex13.java
@@ -0,0 +1,19 @@
+package ex13;
+
+import java.util.Scanner;
+
+public class ex13 {
+	public static void main(String args[]) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Digite um número inteiro: ");
+		int number = scan.nextInt();
+		
+		if(number % 2 == 0) {
+			System.out.printf("O número %d é par!", number);
+		} else {
+			System.out.printf("O número %d é ímpar!", number);
+		}
+		scan.close();
+	}
+}
